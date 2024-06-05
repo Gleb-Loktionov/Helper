8c4877275feae15c3a01710c248a7710c6b8e6ee
Gleb <neloxchestno@gmail.com>
2024-06-05 23:01:26 +0600

    Get user agent

--- helper/main/views.py
+++ helper/main/views.py
@@ -1,6 +1,13 @@
+from typing import Any
 from django.shortcuts import render
 from django.views.generic import TemplateView
 
 
 class MainPage(TemplateView):
     template_name = 'main.html'
+
+    def get_context_data(self, **kwargs: Any) -> dict[str, Any]:
+        context = super().get_context_data(**kwargs)
+        context['agent'] = self.request.META['HTTP_USER_AGENT']
+
+        return context
